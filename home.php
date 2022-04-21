261c14540f4d3e144903aa98d66c6f9ede8d5045
haslozik <wojciechperlinskitpel@gmail.com>
2022-04-21 17:47:42 +0200

    update

--- home.php
+++ home.php
@@ -58,6 +58,69 @@
         </h1>
     </section>
 
+    <section class="finderSection">
+        <h1 class="finderText">Below you can find players announcements</h1>
+        <h5 class="finderInfoText">If you want to add an announcement you must be logged in!</h5>
+        <br><hr>
+    </section>
+
+    <section class="announcementsSection">
+        <div class="announcementElement">
+            <div class="announcementImage"></div>
+            <h3 class="announcementName">
+                Nickname1
+            </h3>
+            <h4 class="announcementRank">
+                Diamond II
+            </h4>
+        </div>
+        <div class="announcementElement">
+            <div class="announcementImage"></div>
+            <h3 class="announcementName">
+                Nickname2
+            </h3>
+            <h4 class="announcementRank">
+                Gold IV
+            </h4>
+        </div>
+        <div class="announcementElement">
+            <div class="announcementImage"></div>
+            <h3 class="announcementName">
+                Nickname3
+            </h3>
+            <h4 class="announcementRank">
+                Silver III
+            </h4>
+        </div>
+        <div class="announcementElement">
+            <div class="announcementImage"></div>
+            <h3 class="announcementName">
+                Nickname1
+            </h3>
+            <h4 class="announcementRank">
+                Diamond II
+            </h4>
+        </div>
+        <div class="announcementElement">
+            <div class="announcementImage"></div>
+            <h3 class="announcementName">
+                Nickname2
+            </h3>
+            <h4 class="announcementRank">
+                Gold IV
+            </h4>
+        </div>
+        <div class="announcementElement">
+            <div class="announcementImage"></div>
+            <h3 class="announcementName">
+                Nickname3
+            </h3>
+            <h4 class="announcementRank">
+                Silver III
+            </h4>
+        </div>
+    </section>
+
     <footer>
         <h1 class="logo">
             <a href="index.php"><sup>&copy;</sup>lolteamup</a>
