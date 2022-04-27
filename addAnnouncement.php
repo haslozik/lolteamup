9227b21185e9c3d394fb76fe6866904e2359602d
haslozik <wojciechperlinskitpel@gmail.com>
2022-04-27 12:03:58 +0200

    update

--- addAnnouncement.php
+++ addAnnouncement.php
@@ -28,3 +28,85 @@
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>lolteamup - sign up</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-sign-pages.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a821291b86.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav>
+        <h1 class="logo">
+            <a href="index.php" class="logoLink">lolteamup</a>
+        </h1>
+        <div class="navBtnsContainer">
+            <a class="navSignupBtn" href="home.php">
+                Home
+            </a>
+        </div>
+    </nav>
+
+    <form method="POST" action="addAnnouncement.php">
+        <div class="signUpForm">
+        <h2 class="formTitle">Add an announcement</h2>
+            <input type="text" placeholder="League of Legends Nickname" name="nickname" required><br>
+            <select name="lane">
+                <option value="" selected disabled hidden>Choose your lane</option>
+                <option value="Top">Top</option>
+                <option value="Jungle">Jungle</option>
+                <option value="Middle">Middle</option>
+                <option value="ADC">ADC</option>
+                <option value="Support">Support</option>
+            </select>    
+            <br>
+            <select name="rank">
+                <option value="" selected disabled hidden>Choose your rank</option>
+                <option value="Iron IV">Iron IV</option>
+                <option value="Iron III">Iron III</option>
+                <option value="Iron II">Iron II</option>
+                <option value="Iron I">Iron I</option>
+                <option value="" disabled></option>
+                <option value="Silver IV">Silver IV</option>
+                <option value="Silver III">Silver III</option>
+                <option value="Silver II">Silver II</option>
+                <option value="Silver I">Silver I</option>
+                <option value="" disabled></option>
+                <option value="Gold IV">Gold IV</option>
+                <option value="Gold III">Gold III</option>
+                <option value="Gold II">Gold II</option>
+                <option value="Gold I">Gold I</option>
+                <option value="" disabled></option>
+                <option value="Platinum IV">Platinum IV</option>
+                <option value="Platinum III">Platinum III</option>
+                <option value="Platinum II">Platinum II</option>
+                <option value="Platinum I">Platinum I</option>
+                <option value="" disabled></option>
+                <option value="Diamond IV">Diamond IV</option>
+                <option value="Diamond III">Diamond III</option>
+                <option value="Diamond II">Diamond II</option>
+                <option value="Diamond I">Diamond I</option>
+                <option value="" disabled></option>
+                <option value="Master">Master</option>
+                <option value="Grandmaster">Grandmaster</option>
+                <option value="Challenger">Challenger</option>
+            </select>    
+            <br>
+            <input type="submit" value="SUBMIT">
+        </div>
+    </form>
+
+    <?php include "footer.php"; ?>
+
+</body>
+</html>
